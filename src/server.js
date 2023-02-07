3c3f866a0ca230130fab2af1e3438fb46ef7dcfa
greenbookwebb <51996455+greenbookwebb@users.noreply.github.com>
2023-02-07 15:28:30 +1000

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -2,26 +2,17 @@ const express = require('express');
 const axios = require('axios');
 const request = require('request');
 const cors = require('cors');
-const app = express();
 const { getLyrics } = require("lyrics-dumper");
+
+const app = express();
+app.use(cors());
 
 const accessToken = 'cojH8jQqn4cF36PceSsnXySHxnoR9h1FDOmI-ACXHY4SBn5tWhEfiqIO_izcqQQr';
 
-app.use(cors());
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.get('/search', (req, res) => {
-  //console.log(req.query);
   let q = req.query;
   let apiUrl = 'https://api.genius.com/search?q=';
   apiUrl = apiUrl.concat(q.q);
-  //console.log(apiUrl);
 
   request({
     url: apiUrl,
@@ -38,12 +29,9 @@ app.get('/search', (req, res) => {
 });
 
 app.get('/song', (req, res) => {
-  console.log("SONG", req.query);
   let q = req.query;
-  console.log(q);
   let apiUrl = 'https://api.genius.com/songs/';
   apiUrl = apiUrl.concat(q.q);
-  console.log(apiUrl);
 
   request({
     url: apiUrl,
@@ -59,27 +47,17 @@ app.get('/song', (req, res) => {
   });
 });
 
-
-
 app.get("/lyrics", (req, res) => {
-    const main = async(artist, song) => {
-        try {
-        //console.log("Artist", artist, "Song", song)
-        const result = await getLyrics(`${artist}, ${song}`);
-        //console.log(result);
-        res.send(result);
-        } catch(error) {
-            console.error(error);
-        }
-    };
+  const main = async(artist, song) => {
+    try {
+      const result = await getLyrics(`${artist}, ${song}`);
+      res.send(result);
+    } catch(error) {
+      console.error(error);
+    }
+  };
 
-    
-    main(req.query.artist, req.query.song);
-
+  main(req.query.artist, req.query.song);
 });
 
-app.listen(3000, () => {
-  console.log("Proxy server listening on port 3000");
-
-
-});
+module.exports = app;
