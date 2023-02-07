aa7b591545eb5eede43f8711f0e2fbe57f903e9c
greenbookwebb <51996455+greenbookwebb@users.noreply.github.com>
2023-02-07 18:00:07 +1000

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const { spawn } = require("child_process");
 const server = spawn("node", ["server.js"]);
 
-exports.handler = async (event, context) => {
+module.exports = async (event, context) => {
   return {
     statusCode: 200,
-    body: "Server running",
+    body: JSON.stringify({ message: "Server running" }),
   };
 };
